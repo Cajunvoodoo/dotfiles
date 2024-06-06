3f7ae07b919c22733a4f678b8e1bc86c5d303dee
Cajunvoodoo <27892784+Cajunvoodoo@users.noreply.github.com>
2024-06-06 10:03:07 -0400

    fix nixd

--- configuration.nix
+++ configuration.nix
@@ -251,6 +251,10 @@ in {
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  environment.sessionVariables = rec {
+    NIXD_FLAGS = "--inlay-hints=true --semantic-tokens=true";
+  };
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
